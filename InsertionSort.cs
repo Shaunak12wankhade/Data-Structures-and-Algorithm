150c1164d88ac3f3aaf01fed87077f3ef3060d44
unknown <shaunak12wankhade@ggmail.com>
2021-10-31 14:26:41 +0530

    [Shaunak] Added : Insertion sort problem

---
+++ InsertionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresandAlgorithms
+{
+    public class InsertionSort
+    {
+        public void Insertion(IComparable[] names)
+        {
+            int i, j;
+
+            for (i = 1; i < names.Length; i++)
+            {
+                IComparable data = names[i];
+                j = i - 1;
+                while ((j >= 0) && (names[j].CompareTo(data) > 0))
+                {
+                    names[j + 1] = names[j];
+                    j--;
+                }
+                names[j + 1] = data;
+            }
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
