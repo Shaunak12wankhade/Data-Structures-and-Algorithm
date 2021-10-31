273e2f4349ce181013cbfc2a36fc18965bf87453
unknown <shaunak12wankhade@ggmail.com>
2021-10-31 14:13:59 +0530

    [Shaunak] Added : Bubble sort problem

--- Program.cs
+++ Program.cs
@@ -8,15 +8,27 @@ namespace DataStructuresandAlgorithms
         {
             Console.WriteLine("Welcome to Datastructures and Algorithms");
 
-            BinarySearchword dataStructure = new BinarySearchword();
-            string[] words = { "decs", "read", "logic", "search" };
-            string data = "search";
-            int result = dataStructure.FindWord(words, data);
-            if (result == -1)
-                Console.WriteLine("Word not present");
 
-            else
-                Console.WriteLine("Word is found at " + result);
+            // BinarySearchWord
+
+            //BinarySearchword dataStructure = new BinarySearchword();
+            //string[] words = { "decs", "read", "logic", "search" };
+            //string data = "search";
+            //int result = dataStructure.FindWord(words, data);
+            //if (result == -1)
+            //    Console.WriteLine("Word not present");
+
+            //else
+            //    Console.WriteLine("Word is found at " + result);
+
+
+
+
+            //BubbleSort
+
+            Console.WriteLine("Data Structure!");
+            Bubblesort dataStructure = new Bubblesort();
+            dataStructure.BubbleSort();
 
         }
     }
